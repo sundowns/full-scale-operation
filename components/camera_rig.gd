995e1de078cbedbf89c628e861a7d6b281cfa047
sundowns <tom@smallridge.com.au>
2024-08-17 09:25:09 +0800

    just terrible really

--- components/camera_rig.gd
+++ components/camera_rig.gd
@@ -1,18 +1,21 @@
 extends Camera3D
 class_name CameraRig
 
-@export var offset: Vector3 = Vector3(0, 5, -5)
+@export var offset: Vector3 = Vector3(0, 6.5, 5)
 
 ## Node to follow
-var target: CameraTarget
+var target: Node3D
 
 func _ready() -> void:
 	set_process(false)
 
-func set_target(new_target: CameraTarget) -> void:
+func set_target(new_target: Node3D) -> void:
 	target = new_target
 	set_process(true)
 
 func _process(_delta: float) -> void:
 	if not target: return
-	look_at_from_position(target.global_position + offset, target.global_position)
+	#print('?')
+	global_position = target.global_position + offset
+	look_at(target.global_position)
+	#look_at_from_position(target.global_position + offset, target.global_position)
--- world.gd
+++ world.gd
@@ -2,8 +2,8 @@ extends Node3D
 class_name World
 
 @onready var camera: CameraRig = $CameraRig
-@onready var camera_target: Marker3D = $CameraTarget
+@onready var player: Player = $Player
 
 func _ready() -> void:
 	DependencyHelper.store("Camera", $CameraRig)
-	camera.set_target(camera_target)
+	camera.set_target($Marker3D)
