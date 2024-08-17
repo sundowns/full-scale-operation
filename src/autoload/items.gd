f6f830907016196aa9caa315dc81a589b2703501
sundowns <tom@smallridge.com.au>
2024-08-17 22:19:59 +0800

    re-enable exclusions

--- src/autoload/items.gd
+++ src/autoload/items.gd
@@ -1,11 +1,18 @@
 extends Node
 
-var all_items: Array[ItemData] = []
+var all_items: Array[ItemData] = [
+	preload("res://items/data/baseball_bat.tres"),
+	preload("res://items/data/axe.tres"),
+	preload("res://items/data/anvil.tres"),
+	preload("res://items/data/bowling_ball.tres"),
+]
 
 const item_data_directory: String = "res://items/data/"
+const load_resources_from_filesystem: bool = false
 
 func _ready() -> void:
-	populate_all_items()
+	if load_resources_from_filesystem:
+		populate_all_items()
 
 func get_random_item(exclusions: Array[ItemData] = []) -> ItemData:
 	var selection: ItemData = null
@@ -14,6 +21,7 @@ func get_random_item(exclusions: Array[ItemData] = []) -> ItemData:
 	return selection
 
 func populate_all_items() -> void:
+	all_items = []
 	var directory: DirAccess = DirAccess.open(item_data_directory) 
 	assert(directory, "Oh no we can't load our items :c")
 	for file in directory.get_files():
