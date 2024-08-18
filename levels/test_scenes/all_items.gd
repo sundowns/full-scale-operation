ccfdbde8431249b75a882c1b4f28c18e3d045f68
sundowns <tom@smallridge.com.au>
2024-08-18 12:42:54 +0800

    more items

--- levels/test_scenes/all_items.gd
+++ levels/test_scenes/all_items.gd
@@ -13,14 +13,12 @@ func _ready() -> void:
 	Callable(spawn_all_items).call_deferred()
 
 func spawn_all_items() -> void:
-	var location := marker_3d.global_position
 	var count: int = 0
 	for item in Items.all_items:
 		var new_item := item_scene.instantiate() as Item
 		new_item.data = item
 		DependencyHelper.retrieve("Items").add_child(new_item)
-		new_item.global_position = location
+		@warning_ignore("integer_division")
+		new_item.global_position = marker_3d.global_position + Vector3(offset * (count % items_per_row), 3, offset * (count / items_per_row))
 		new_item.initialise()
 		count +=1
-		@warning_ignore("integer_division")
-		location = Vector3(offset * (count % items_per_row), 3, offset * (count / items_per_row))
