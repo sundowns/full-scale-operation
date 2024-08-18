ce46617df79c5c158e6f17fb20f07d7e805d69d1
unknown <max.chappos@gmail.com>
2024-08-18 16:17:16 +0800

    freeze on floor baby

--- items/item.gd
+++ items/item.gd
@@ -6,6 +6,7 @@ class_name Item
 @onready var sprite: Sprite3D = $Sprite3D
 @onready var grab_box: GrabBox = $GrabBox
 @onready var weight_component: WeightComponent = $WeightComponent
+@onready var floor_detection: RayCast3D = $CollisionShape3D/RayCast3D
 
 @export var highlight_material: ShaderMaterial = preload("res://vfx/item_highlight_material.tres")
 @export var follow_speed = 15
@@ -15,6 +16,7 @@ class_name Item
 
 var _is_initialised: bool = false
 var is_being_held: bool =false
+var is_on_floor: bool = false
 
 func _ready() -> void:
 	if data:
@@ -33,6 +35,18 @@ func initialise() -> void:
 func _process(delta: float) -> void:
 	if is_being_held:
 		global_position = lerp(global_position, get_parent().global_position, get_follow_speed() * delta)
+	else:
+		if floor_detection.is_colliding():
+			freeze = true
+		else:
+			freeze = false
+		
+
+func _physics_process(delta: float) -> void:
+	if not is_being_held and not is_on_floor:
+		if floor_detection.is_colliding():
+			_on_landing()
+	
 
 func get_follow_speed() -> float:
 	var speed = remap(data.weight, 0, weight_weight,  follow_speed_max, follow_speed_min)
@@ -42,12 +56,19 @@ func _on_pickup() -> void:
 	top_level = true
 	is_being_held = true
 	freeze = true
+	is_on_floor = false
+	floor_detection.enabled = false
 	AudioPlayer.play_sfx(data.grab_sfx)
 
 func _on_drop() -> void:
 	top_level = false
 	is_being_held = false
 	freeze = false
+	floor_detection.enabled = true
+	
+func _on_landing() -> void:
+	freeze = true
+	floor_detection.enabled = false
 	AudioPlayer.play_sfx(data.drop_sfx)
 
 func _mark_as_target_for_pickup() -> void:
