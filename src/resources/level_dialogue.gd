fff0a45ea65207a49037047390a24dc97f381597
sundowns <tom@smallridge.com.au>
2024-08-19 16:15:06 +0800

    dialogue mission complete

--- src/resources/level_dialogue.gd
+++ src/resources/level_dialogue.gd
@@ -4,6 +4,7 @@ class_name LevelDialogue
 @export var label: String = ""
 @export var timeline: DialogicTimeline
 @export var one_shot: bool = true
+@export var pause_game: bool = true
 
 var has_run: bool = false
 
@@ -15,8 +16,7 @@ func play() -> void:
 		return
 	has_run = true
 	Dialogic.start(timeline)
-	Dialogic.process_mode = Node.PROCESS_MODE_ALWAYS
-	DependencyHelper.retrieve("World").pause_game()
-	await Dialogic.timeline_ended
-	DependencyHelper.retrieve("World").unpause_game()
-	print('its joever')
+	if pause_game:
+		DependencyHelper.retrieve("World").pause_game()
+		await Dialogic.timeline_ended
+		DependencyHelper.retrieve("World").unpause_game()
